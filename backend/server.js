0d4cf583de4f648539ee19da3b70219501bc9e8e
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-07-28 20:13:32 +0100

    .env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
-const port = 5000;
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+const port = process.env.PORT || 5000;
+
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
