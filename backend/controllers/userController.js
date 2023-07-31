72f4656e676ed47a73c41c3b888d207fb482c54e
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-07-31 15:11:54 +0100

    Connect to user Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,6 +6,34 @@ const authUser  = asyncHandler(async (req, res) => {
     res.status(200).json({messzge : 'Auth User'})
 });
 
+//          Register a new user
+//          POST /api/users
+const registerUser  = asyncHandler(async (req, res) => {  
+    res.status(200).json({messzge : 'Register User'})
+});
+
+//          Logout user
+//          POST /api/users/logout
+const logoutUser  = asyncHandler(async (req, res) => {  
+    res.status(200).json({messzge : 'Logout User'})
+});
+
+//          Get user profile
+//          GET /api/users/profile
+const getUserProfile  = asyncHandler(async (req, res) => {  
+    res.status(200).json({messzge : 'Profile User'})
+});
+
+//          Update user profile
+//          PUT /api/users/profile
+const updateUserProfile  = asyncHandler(async (req, res) => {  
+    res.status(200).json({messzge : 'Update User Profile'})
+});
+
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 };
