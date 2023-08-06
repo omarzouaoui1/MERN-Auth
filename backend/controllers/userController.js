9dd542cc9e6148df0865a8c4ff23346f661f27d0
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-08-06 09:38:28 +0100

    Update User Profile Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,7 +81,27 @@ const getUserProfile  = asyncHandler(async (req, res) => {
 //          Update user profile
 //          PUT /api/users/profile
 const updateUserProfile  = asyncHandler(async (req, res) => {  
-    res.status(200).json({messzge : 'Update User Profile'})
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password){
+            user.password = user.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+        })
+    } else{
+        res.status(404);
+        throw new Error('User not found')
+    }
 });
 
 export {
