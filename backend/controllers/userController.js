4aa09ecd5f0e4dbd3fcaa006f78148799bc2d05a
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-08-06 09:00:17 +0100

    Auth User Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,23 @@ import generateToken from '../utils/generateToken.js';
 //          Auth user/set token
 //          POST /api/users/auth
 const authUser  = asyncHandler(async (req, res) => {  
-    res.status(200).json({messzge : 'Auth User'})
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(user && (await user.matchPassword(password))){
+        generateToken(res, user._id);
+        
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else{
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+
 });
 
 //          Register a new user
