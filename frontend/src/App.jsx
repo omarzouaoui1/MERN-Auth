27c3ab7b4bc36ce6901e8303e010c920d55d0fc5
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-08-07 12:40:56 +0100

    NavBar & working on HomeScreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
   return(
     <>
-    <h1>My App</h1>
+    <Header/>
+    <HomeScreen/>
     </>
   )
 }
