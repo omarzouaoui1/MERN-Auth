bfd26e39fbe153b9f28b4d469fe1ec14419ed403
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-08-09 14:57:06 +0100

    Redux Toolkit Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,8 @@ import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LogInScreen from './screens/LogInScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
-
+import store from './store.js'
+import { Provider } from 'react-redux'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,7 +30,9 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
     <RouterProvider router={router}/>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>
 )
