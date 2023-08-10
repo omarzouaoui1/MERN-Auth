ae19d3939366dd21bff2792522d5ac15fb18a976
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-08-10 12:56:05 +0100

    Auth Slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
 
 const store = configureStore({
-    reducer: {},
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-    devTools: true,
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
 });
 
 export default store;
